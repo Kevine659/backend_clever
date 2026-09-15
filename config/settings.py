ed4119a93568c3fff8ee85e9bb5161c0b3632e2e
kevine659 <kevinemafouomenekenne@gmail.com>
2026-09-15 14:59:57 +0200

    modification

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework.authtoken",
+    "rest_framework",
     "corsheaders",
     "products",
     "accounts",
